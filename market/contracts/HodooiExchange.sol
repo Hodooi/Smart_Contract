42b06b35c150795734f17fae629c2511f2dbe1ac
Johnny Hoang <tuan.hoang2@sotatek.com>
2021-06-16 13:58:47 +0700

    update exchange

--- market/contracts/HodooiExchange.sol
+++ market/contracts/HodooiExchange.sol
@@ -8,18 +8,40 @@ import "./interfaces/IBSCswapRouter.sol";
 contract HodooiExchange is Ownable {
     address public bnbRouter;
     address public usdt;
+    address public usdtMarket;
     address public busd;
     address public bnb;
 
     constructor(address _bnbRouter, address _usdt, address _busd, address _bnb) public {
         bnbRouter = _bnbRouter;
         usdt = _usdt;
+        usdtMarket = _usdt;
         busd = _busd;
         bnb = _bnb;
     }
 
     function setBnbRouter(address _bnbRouter) onlyOwner public returns (bool) {
         bnbRouter = _bnbRouter;
+        return true;
+    }
+
+    function setUsdtMarket(address _usdt) onlyOwner public returns (bool) {
+        usdtMarket = _usdt;
+        return true;
+    }
+
+    function setUsdt(address _usdt) onlyOwner public returns (bool) {
+        usdt = _usdt;
+        return true;
+    }
+
+    function setBnb(address _bnb) onlyOwner public returns (bool) {
+        bnb = _bnb;
+        return true;
+    }
+
+    function setBusd(address _busd) onlyOwner public returns (bool) {
+        busd = _busd;
         return true;
     }
 
@@ -59,7 +81,7 @@ contract HodooiExchange is Ownable {
     function estimateToUSDT(address _paymentToken, uint256 _paymentAmount) public view returns (uint256) {
         uint256[] memory amounts;
         uint256 result;
-        if (_paymentToken != usdt) {
+        if (_paymentToken != usdt && _paymentToken != usdtMarket) {
             address[] memory path;
             uint256 amountIn = _paymentAmount;
             if (_paymentToken == address(0)) {
@@ -85,7 +107,7 @@ contract HodooiExchange is Ownable {
     function estimateFromUSDT(address _paymentToken, uint256 _usdtAmount) public view returns (uint256) {
         uint256[] memory amounts;
         uint256 result;
-        if (_paymentToken != usdt) {
+        if (_paymentToken != usdt && _paymentToken != usdtMarket) {
             address[] memory path;
             uint256 amountIn = _usdtAmount;
             if (_paymentToken == address(0)) {
--- market/scripts/deploy-market-only.script.js
+++ market/scripts/deploy-market-only.script.js
@@ -1,8 +1,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const exchange = "0x88e4C6808669ae32e8E0E242230F7212DfaBAE60";
-  const referral = "0x89EBf09B55CD85119C48254eE04B522d6e7fd72d";
+  // const exchange = "0x88e4C6808669ae32e8E0E242230F7212DfaBAE60";
+  // const referral = "0x89EBf09B55CD85119C48254eE04B522d6e7fd72d";
+  // const usdt = "0x14ec6EE23dD1589ea147deB6c41d5Ae3d6544893";
+  const exchange = "0xbe3050809Aff0667836944233BAEAB7977aC39eD";
+  const referral = "0xa9fE22967de8B476725296cB2690C07c665cF787";
   const usdt = "0x14ec6EE23dD1589ea147deB6c41d5Ae3d6544893";
   /// DEPLOY MARKET
   const HodooiMarket = await hre.ethers.getContractFactory("HodooiMarket");
